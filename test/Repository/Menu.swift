be17bc0abb3529bb8d5444bdf0ce5e752e784a98
SuZu008006 <suzusuke0806@gmail.com>
2021-12-16 21:16:34 +0900

    minor correction to MenuDB

--- test/Repository/Menu.swift
+++ test/Repository/Menu.swift
@@ -2,7 +2,8 @@ struct Menu {
 
     let title: String
     let ingredients: Dictionary<String,String>
-    let seasoning: Dictionary<String,String>
+    let seasonings: Dictionary<String,String>
+    let directions: [String]
     let category: String
     let image: String
     
@@ -13,64 +14,154 @@ extension Menu{
     static var MenuDB = [
         Menu(
             title: "魚を焼いたもの",
-            ingredients: ["魚" : "100[g]","魚のような何か" : "200[g]"],
-            seasoning: ["塩" : "30[g]","海水" : "10[ml]"],
+            ingredients: [
+                "魚" : "100[g]",
+                "魚のような何か" : "200[g]"
+            ],
+            seasonings: [
+                "塩" : "30[g]",
+                "海水" : "10[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "さかなぁ",
             image: "image2"
         ),
         Menu(
             title: "肉を焼いたもの",
-            ingredients: ["肉" : "100[g]","肉のような何か" : "300[g]"],
-            seasoning: ["醤油" : "20[ml]","海水" : "10[ml]"],
+            ingredients: [
+                "肉" : "100[g]",
+                "肉のような何か" : "300[g]"
+            ],
+            seasonings: [
+                "醤油" : "20[ml]",
+                "海水" : "10[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "にくぅ",
             image: "image2"
         ),
         Menu(
             title: "野菜を焼いたもの",
-            ingredients: ["野菜" : "150[g]","野菜のような何か" : "300[g]"],
-            seasoning: ["味噌" : "5[g]" ,"海水" : "100[ml]"],
+            ingredients: [
+                "野菜" : "150[g]",
+                "野菜のような何か" : "300[g]"
+            ],
+            seasonings: [
+                "味噌" : "5[g]",
+                "海水" : "100[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "やさぁ",
             image: "image3"
         ),
         Menu(
             title: "魚を焼いたもの",
-            ingredients: ["魚" : "100[g]","魚のような何か" : "200[g]"],
-            seasoning: ["塩" : "30[g]","海水" : "10[ml]"],
+            ingredients: [
+                "魚" : "100[g]",
+                "魚のような何か" : "200[g]"
+            ],
+            seasonings: [
+                "塩" : "30[g]",
+                "海水" : "10[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "さかなぁ",
             image: "image2"
         ),
         Menu(
             title: "肉を焼いたもの",
-            ingredients: ["肉" : "100[g]","肉のような何か" : "300[g]"],
-            seasoning: ["醤油" : "20[ml]","海水" : "10[ml]"],
+            ingredients: [
+                "肉" : "100[g]",
+                "肉のような何か" : "300[g]"
+            ],
+            seasonings: [
+                "醤油" : "20[ml]",
+                "海水" : "10[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "にくぅ",
             image: "image2"
         ),
         Menu(
             title: "野菜を焼いたもの",
-            ingredients: ["野菜" : "150[g]","野菜のような何か" : "300[g]"],
-            seasoning: ["味噌" : "5[g]" ,"海水" : "100[ml]"],
+            ingredients: [
+                "野菜" : "150[g]",
+                "野菜のような何か" : "300[g]"
+            ],
+            seasonings: [
+                "味噌" : "5[g]",
+                "海水" : "100[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "やさぁ",
             image: "image3"
         ),
         Menu(
             title: "魚を焼いたもの",
-            ingredients: ["魚" : "100[g]","魚のような何か" : "200[g]"],
-            seasoning: ["塩" : "30[g]","海水" : "10[ml]"],
+            ingredients: [
+                "魚" : "100[g]",
+                "魚のような何か" : "200[g]"
+            ],
+            seasonings: [
+                "塩" : "30[g]",
+                "海水" : "10[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "さかなぁ",
             image: "image2"
         ),
         Menu(
             title: "肉を焼いたもの",
-            ingredients: ["肉" : "100[g]","肉のような何か" : "300[g]"],
-            seasoning: ["醤油" : "20[ml]","海水" : "10[ml]"],
+            ingredients: [
+                "肉" : "100[g]",
+                "肉のような何か" : "300[g]"
+            ],
+            seasonings: [
+                "醤油" : "20[ml]",
+                "海水" : "10[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "にくぅ",
             image: "image2"
         ),
         Menu(
             title: "野菜を焼いたもの",
-            ingredients: ["野菜" : "150[g]","野菜のような何か" : "300[g]"],
-            seasoning: ["味噌" : "5[g]" ,"海水" : "100[ml]"],
+            ingredients: [
+                "野菜" : "150[g]",
+                "野菜のような何か" : "300[g]"
+            ],
+            seasonings: [
+                "味噌" : "5[g]",
+                "海水" : "100[ml]"
+            ],
+            directions: [
+                "焼く",
+                "盛り付ける"
+            ],
             category: "やさぁ",
             image: "image3"
         ),
